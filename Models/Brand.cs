9052ffc177c55034c8815ee932621d4265035099
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-07-16 06:05:07 -0300

    Create entity brand

--- Models/Brand.cs
+++ Models/Brand.cs
@@ -1,6 +1,31 @@
-﻿namespace StockControlApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace StockControlApi.Models
 {
     public class Brand
     {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public Guid UserId { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+        public bool Active { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+
+        [JsonIgnore]
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; } = null!;
     }
 }
