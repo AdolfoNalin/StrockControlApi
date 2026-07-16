c8ab6222847da7ecd9b4fdbf6730d1103103a908
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-07-15 21:58:10 -0300

    Create Entity responsible for movementation the of stock

---
+++ Models/StockMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockControlApi.Models
+{
+    public enum MovimentType
+    {
+        Entry,
+        Exit,
+        Ajustment
+    }
+
+    public class StockMovement
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public Guid ProductId { get; set; }
+
+        public Guid UserId { get; set; }
+
+        [Required(ErrorMessage = "O campo tipo de movimentação está vazio")]
+        public MovimentType MovimentType { get; set; }
+
+        [Required(ErrorMessage = "O campo Quanitade está vazio")]
+        [Range(1, int.MaxValue, ErrorMessage = "Digite o valor correto")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "O campo unitário é obrigatório")]
+        [Range(1, int.MaxValue)]
+        public decimal UnitPrice { get; set; }
+
+        [NotMapped]
+        public decimal TotalValue  => Quantity * UnitPrice;
+
+        [MaxLength(500)]
+        public string? Observation { get; set; }
+
+        [Required(ErrorMessage = "O campo movimentação precisa de Data")]
+        [DataType(DataType.Date)]
+        public DateTime MovementDate { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey(nameof(ProductId))]
+        public Product? Product { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+    }
+}
