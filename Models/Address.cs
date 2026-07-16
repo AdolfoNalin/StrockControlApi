eeb86d2459f23e997f5ee23ac8dae03e775c4e85
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-07-15 21:31:49 -0300

    Create class resposible for address

---
+++ Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockControlApi.Models
+{
+    [NotMapped]
+    public class Address
+    {
+        public string Street { get; set; } = string.Empty;
+        public string Number { get; set; } = string.Empty;
+        public string Complement { get; set; } = string.Empty;
+        public string District { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+    }
+}
