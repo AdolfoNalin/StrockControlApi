7d9f550e509a1278105376804805632b8c762d54
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-07-15 22:10:39 -0300

    Create the entity Category

---
+++ Models/Category.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockControlApi.Models
+{
+    public class Category
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public Guid UserId { get; set; }
+
+        [Required(ErrorMessage = "O campo nome está vazio")]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [MaxLength(150)]
+        public string? Description { get; set; }
+
+        public bool Active { get; set; } = true;
+
+        [Required(ErrorMessage = "O campo Data está vazio")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User? User { get; set; }
+    }
+}
