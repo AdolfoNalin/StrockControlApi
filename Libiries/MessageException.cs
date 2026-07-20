d9245b8dd16056bc6998ed8e0ca56116abc85a70
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-07-19 21:16:40 -0300

    Create class MessageException

---
+++ Libiries/MessageException.cs
@@ -0,0 +1,17 @@
+﻿namespace StockControlApi.Libiries
+{
+    public class MessageException
+    {
+        #region MessageBadRequest
+        /// <summary>
+        /// Function responsable for settings the message BadRequest
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns>String format</returns>
+        public static string MessageBadRequest(Exception ex)
+        {
+            return $"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}";
+        }
+        #endregion
+    }
+}
