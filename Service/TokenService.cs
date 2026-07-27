94ea63fdddb7f43d584176525dae3b5f8a3bc7ab
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-07-26 21:09:16 -0300

    Create class TokenService

---
+++ Service/TokenService.cs
@@ -0,0 +1,59 @@
+﻿using StockControlApi.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace StockControlApi.Services
+{
+    public class TokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        #region GenerateToken
+        /// <summary>
+        /// Method resposable for generate token for user login
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public string GenerateToken(User user)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_configuration["Key"] ?? throw new ArgumentNullException("Argumento não encontrado!"));
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(
+                        new Claim[]
+                        {
+                        new Claim(ClaimTypes.NameIdentifier, user.Name.ToString()),
+                        new Claim(ClaimTypes.Role, user.PasswordHash.ToString()),
+                        }),
+                    Expires = DateTime.UtcNow.AddHours(2),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+
+                };
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                return tokenHandler.WriteToken(token);
+            }
+            catch (ArgumentNullException an)
+            {
+                return an.Message;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+        }
+        #endregion
+    }
+}
