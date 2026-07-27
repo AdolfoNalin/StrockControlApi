50172db34ceead162ed0aa59ded27d7d7c5069fc
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-07-26 21:05:48 -0300

    Create class users parameter

---
+++ Models/UserLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace StockControlApi.Models
+{
+    public class UserLogin
+    {
+        public string Login { get; set; }
+        public string PasswordHass { get; set; }
+    }
+}
---
+++ Models/UserResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace StockControlApi.Models
+{
+    public class UserResponse
+    {
+        public Guid UserId { get; set; }
+        public string Login { get; set; }
+        public string Token { get; set; }
+    }
+}
