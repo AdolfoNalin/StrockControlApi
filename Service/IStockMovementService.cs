ce110f7265d490979f93787d6e87e59fc9b01a9f
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-07-26 21:11:51 -0300

    Create class IStockMovementService

---
+++ Service/IStockMovementService.cs
@@ -0,0 +1,15 @@
+﻿using StockControlApi.Models;
+
+namespace StockControlApi.Service
+{
+    public interface IStockMovementService
+    {
+        public Task<List<StockMovement>> GetAll();
+        public Task<StockMovement> GetById(Guid id);
+        public Task<List<StockMovement>> GetByProductId(Guid id);
+        public Task<List<StockMovement>> GetSmartSearch(string value);
+        public Task<List<StockMovement>> GetByDate(DateTime? startDate, DateTime? endDate);
+        public Task<bool> Create(StockMovement stockMovement);
+        public Task<bool> Update(StockMovement stockMovement);
+    }
+}
