22409f50bff85a456c8aaa0c73ce2c69ea67f33a
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-08-09 08:49:57 -0300

    habitando swagger em produção

--- Program.cs
+++ Program.cs
@@ -54,11 +54,10 @@ builder.Services.AddSingleton<TokenService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
